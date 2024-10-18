774c004b4a00adb95748fc2db2e5a59728742078
Ruan Caetano <ruansouza_caetano@hotmail.com>
2024-10-17 21:32:02 -0300

    feat: solve 1.8

---
+++ golang/chapter1/ex1_8.go
@@ -0,0 +1,58 @@
+package chapter1
+
+import (
+	"fmt"
+)
+
+// Run1_8
+// Zero Matrix: Write an algorithm such that if an element in an MxN matrix is 0, its entire row and column are set to 0.
+//
+// Input
+// [
+//
+//	[1,2,3]
+//	[4,5,6]
+//	[7,8,0]
+//
+// ]
+//
+// Output
+// [
+//
+//	[1,2,0]
+//	[4,5,0]
+//	[0,0,0]
+//
+// ]
+func Run1_8() {
+	fmt.Println(solveZeroMatrix([][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 0}}))
+}
+
+// solveZeroMatrix
+// time O(NxM)
+// storage O(N+M)
+func solveZeroMatrix(matrix [][]int) [][]int {
+	n := len(matrix)
+	m := len(matrix[0])
+
+	zerosRows := make([]bool, n)
+	zeroColumns := make([]bool, n)
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			if matrix[i][j] == 0 {
+				zerosRows[i] = true
+				zeroColumns[j] = true
+			}
+		}
+	}
+
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			if zerosRows[i] || zeroColumns[j] {
+				matrix[i][j] = 0
+			}
+		}
+	}
+
+	return matrix
+}
--- golang/main.go
+++ golang/main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	chapter1.Run1_6()
+	chapter1.Run1_8()
 }
